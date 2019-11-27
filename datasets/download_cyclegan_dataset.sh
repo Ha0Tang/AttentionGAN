7710212d805899d5c5267bec460712cacd120ef7
Ha0Tang <haotang@pku.edu.cn>
2019-11-27 17:33:33 +0000

    update code

---
+++ datasets/download_cyclegan_dataset.sh
@@ -0,0 +1,16 @@
+FILE=$1
+
+if [[ $FILE != "apple2orange" && $FILE != "horse2zebra" && $FILE != "maps" ]]; then
+    echo "Available datasets are: apple2orange, horse2zebra, maps"
+    exit 1
+fi
+
+
+echo "Specified [$FILE]"
+URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
+ZIP_FILE=./datasets/$FILE.zip
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $ZIP_FILE
+mkdir $TARGET_DIR
+unzip $ZIP_FILE -d ./datasets/
+rm $ZIP_FILE
---
+++ scripts/download_attentiongan_model.sh
@@ -0,0 +1,14 @@
+FILE=$1
+
+echo "Note: available models are apple2orange, horse2zebra, photo2map, map2photo"
+echo "Specified [$FILE]"
+
+URL=http://disi.unitn.it/~hao.tang/uploads/models/AttentionGAN/${FILE}_pretrained.tar.gz
+TAR_FILE=./checkpoints/${FILE}_pretrained.tar.gz
+TARGET_DIR=./checkpoints/${FILE}_pretrained/
+
+wget -N $URL -O $TAR_FILE
+
+mkdir -p $TARGET_DIR
+tar -zxvf $TAR_FILE -C ./checkpoints/
+rm $TAR_FILE
