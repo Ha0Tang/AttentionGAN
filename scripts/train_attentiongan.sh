579aee6e79b95bb2260e37455c84c7ab3c8c51e9
Hao Tang <hao.tang@unitn.it>
2019-12-01 12:47:50 +0000

    update train script

--- scripts/train_attentiongan.sh
+++ scripts/train_attentiongan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/horse2zebra --name horse2zebra_attentiongan --model attention_gan --dataset_mode unaligned --pool_size 50 --no_dropout --norm instance --lambda_A 10 --lambda_B 10 --lambda_identity 0.5 --load_size 286 --crop_size 256 --batch_size 1 --gpu_ids 0 --display_id 0 --display_freq 1 --print_freq 1;
+python train.py --dataroot ./datasets/horse2zebra --name horse2zebra_attentiongan --model attention_gan --dataset_mode unaligned --pool_size 50 --no_dropout --norm instance --lambda_A 10 --lambda_B 10 --lambda_identity 0.5 --load_size 286 --crop_size 256 --batch_size 1 --gpu_ids 0 --display_id 0 --display_freq 100 --print_freq 100;
