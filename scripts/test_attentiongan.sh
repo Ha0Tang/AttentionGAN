a4b76d921bf9691692156d723fbd3f8d44dd91ef
Ha0Tang <haotang@pku.edu.cn>
2019-12-04 20:28:51 +0000

    update test code

--- scripts/test_attentiongan.sh
+++ scripts/test_attentiongan.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/horse2zebra --name horse2zebra_attentiongan --model attention_gan --dataset_mode unaligned --norm instance --phase test --no_dropout --load_size 256 --crop_size 256 --batch_size 1 --gpu_ids 0 --num_test 1000000000;
+python test.py --dataroot ./datasets/horse2zebra --name horse2zebra_attentiongan --model attention_gan --dataset_mode unaligned --norm instance --phase test --no_dropout --load_size 256 --crop_size 256 --batch_size 1 --gpu_ids 0 --num_test 1000000000 --saceDisk;
